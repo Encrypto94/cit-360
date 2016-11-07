d8da2d09860e79b8f8cfd9055d71382835ca8004
Encrypto94 <encrypto94@gmail.com>
2016-11-06 20:24:31 -0800

    Changed to local ssh traffic

--- terraform/infra.tf
+++ terraform/infra.tf
@@ -208,7 +208,7 @@ resource "aws_security_group" "allow_ssh" {
       from_port = 22
       to_port = 22
       protocol = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
+      cidr_blocks = ["130.166.220.254/32"]
   }
 
   tags {
